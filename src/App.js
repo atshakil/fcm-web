97be8e20e72d8333aae76f7d7554f7bd2c524ba8
Tahmid Shakil <at.shakil.92@gmail.com>
2018-09-04 14:40:05 +0700

    FCM configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+import { FCM_CONFIG } from './config.js';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp(FCM_CONFIG);
+    window.messaging = firebase.messaging();
+    window.messaging.usePublicVapidKey(process.env.REACT_APP_WEB_PUSH_CERT_PUBKEY);
+    window.messaging.requestPermission()
+      .then(() => {console.log('Notification permission granted.')})
+      .catch(err => {console.log('Unable to get permission to notify.', err)});
+    window.messaging.getToken()
+      .then(currentToken => {console.log(currentToken ? `TOKEN READY TO STORE AND SEND TO SERVER: ${currentToken}` : 'No Instance ID token available. Request permission to generate one.')})
+      .catch(err => {console.log('An error occurred while retrieving token. ', err)});
+    window.messaging.onTokenRefresh(() => {
+      console.log(`Token refreshing ...`);
+      window.messaging.getToken()
+        .then(newToken => {
+          console.log(`Token refreshed.`);
+          console.log(`New Token: ${newToken}`);
+        })
+        .catch(e => {
+          console.log(`Token retrival (on refresh) failed. Error: ${e}`);
+        })
+    });
+    window.messaging.onMessage(payload => {
+      console.log(`Message received:`);
+      console.log(payload);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Rendered
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(reg => console.log(`FCM service worker registered successfully. Registration scope: ${reg.scope}`))
+    .catch(e => console.log(`FCM service worker could not be registered. Error: ${e}`));
+}
