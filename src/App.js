e4129e956337a8b301612c8b5cb25df46859ef05
Tahmid Shakil <at.shakil.92@gmail.com>
2018-09-06 19:15:20 +0700

    RTDB support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import _ from 'lodash';
 import { FCM_CONFIG } from './config.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      messages: []
+    };
+    this.enableFCM = this.enableFCM.bind(this);
+    this.enableRTDB = this.enableRTDB.bind(this);
+    this.sendNewMessage = this.sendNewMessage.bind(this);
+    this.clearMessages = this.clearMessages.bind(this);
+  }
+
   componentDidMount() {
     firebase.initializeApp(FCM_CONFIG);
+    window.firebase = firebase;
     window.messaging = firebase.messaging();
+    window.database = firebase.database();
+    window.messages = window.database.ref('messages');
+    this.enableFCM();
+    this.enableRTDB();
+  }
+
+  enableFCM() {
     window.messaging.usePublicVapidKey(process.env.REACT_APP_WEB_PUSH_CERT_PUBKEY);
     window.messaging.requestPermission()
       .then(() => {console.log('Notification permission granted.')})
@@ -31,10 +52,55 @@ class App extends Component {
     });
   }
 
+  enableRTDB() {
+    const msgs = this.state.messages;
+    // register an event at a specific location
+    window.messages.on('child_added', snap => {
+      this.setState({messages: this.state.messages.concat(snap.val())});
+      console.log(`New message added. Message data: ${JSON.stringify(snap.val())}`);
+    })
+    window.messages.on('child_removed', snap => {
+      const
+        index = _.findIndex(msgs, nth => nth.text ===snap.val().text),
+        _messages = msgs.filter((msg, _index) => _index !== index);
+
+      this.setState({messages: _messages});
+      console.log(`A message removed. Message data: ${JSON.stringify(snap.val())}`);
+    })
+  }
+
+  sendNewMessage() {
+    // Create new message
+    const text = this.state.value;
+    if(text === '') return;
+    window.messages.push().set({ text })
+      .catch(e => console.log(`Could not write to RTDB. Error: ${e}`));
+    this.input.value = '';
+    this.setState({value: ''});
+  }
+
+  clearMessages() {
+    window.messages.remove().catch(e => console.log(`Could not clear messsages. Error: ${e}`));
+  }
+
   render() {
     return (
       <div className="App">
-        Rendered
+        <div>Rendered</div>
+        <div>
+          <input ref={node => this.input = node} type="text" onChange={e => this.setState({value: e.target.value})} />
+          <button onClick={e => this.sendNewMessage()}>SEND</button>
+          <button onClick={e => this.clearMessages()}>CLEAR</button>
+        </div>
+        <br /><br /><br />
+        <div>Messages</div>
+        <div>----------------------</div>
+        <div>
+          {this.state.messages.map((msg, index) =>
+            <div key={index}>{msg.text}</div>
+          )}
+        </div>
+
       </div>
     );
   }
