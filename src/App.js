cbb276a9264ea289c1fbc240368430efbb77eec6
Tahmid Shakil <at.shakil.92@gmail.com>
2018-09-10 15:16:19 +0700

    Authentication support added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: '',
+      password: '',
+      isLoggedIn: false,
       value: '',
       messages: []
     };
+    this.authenticateFirebase = this.authenticateFirebase.bind(this);
     this.enableFCM = this.enableFCM.bind(this);
     this.enableRTDB = this.enableRTDB.bind(this);
+    this.signup = this.signup.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
     this.sendNewMessage = this.sendNewMessage.bind(this);
     this.clearMessages = this.clearMessages.bind(this);
   }
@@ -20,11 +27,21 @@ class App extends Component {
   componentDidMount() {
     firebase.initializeApp(FCM_CONFIG);
     window.firebase = firebase;
+    window.auth = firebase.auth();
     window.messaging = firebase.messaging();
     window.database = firebase.database();
     window.messages = window.database.ref('messages');
+    this.authenticateFirebase();
     this.enableFCM();
     this.enableRTDB();
+  }
+
+  authenticateFirebase() {
+    window.auth.onAuthStateChanged(user => {
+      this.setState({ isLoggedIn: !!user })
+      console.log(user ? 'User is now logged in' : 'User is now logged out');
+
+    });
   }
 
   enableFCM() {
@@ -69,6 +86,28 @@ class App extends Component {
     })
   }
 
+  signup() {
+    const { email, password } = this.state;
+    window.auth.createUserWithEmailAndPassword(email, password).catch(e => console.log(e));
+    this.emailField.value = '';
+    this.passwordField.value = '';
+    this.setState({ email: '', password: '' });
+  }
+
+  login() {
+    const { email, password } = this.state;
+    window.auth.signInWithEmailAndPassword(email, password).catch(e => console.log(e));
+    this.emailField.value = '';
+    this.passwordField.value = '';
+    this.setState({ email: '', password: '' });
+  }
+
+  logout() {
+    window.auth.signOut()
+      .then(() => {})
+      .catch(e => console.log(e))
+  }
+
   sendNewMessage() {
     // Create new message
     const text = this.state.value;
@@ -84,23 +123,40 @@ class App extends Component {
   }
 
   render() {
+    const { isLoggedIn } = this.state;
+
     return (
       <div className="App">
-        <div>Rendered</div>
-        <div>
-          <input ref={node => this.input = node} type="text" onChange={e => this.setState({value: e.target.value})} />
-          <button onClick={e => this.sendNewMessage()}>SEND</button>
-          <button onClick={e => this.clearMessages()}>CLEAR</button>
-        </div>
-        <br /><br /><br />
-        <div>Messages</div>
-        <div>----------------------</div>
-        <div>
-          {this.state.messages.map((msg, index) =>
-            <div key={index}>{msg.text}</div>
-          )}
-        </div>
-
+        {isLoggedIn ?
+          <div>
+            <div>Rendered</div>
+            <br />
+            <button onClick={e => this.logout()}>LOGOUT</button>
+            <br /><hr /><br />
+            <div>
+              <input ref={node => this.input = node} type="text" onChange={e => this.setState({value: e.target.value})} />
+              <button onClick={e => this.sendNewMessage()}>SEND</button>
+              <button onClick={e => this.clearMessages()}>CLEAR</button>
+            </div>
+            <br /><br /><br />
+            <div>Messages</div>
+            <div>----------------------</div>
+            <div>
+              {this.state.messages.map((msg, index) =>
+                <div key={index}>{msg.text}</div>
+              )}
+            </div>
+          </div>
+          :
+          <div>
+            <div>Email: <input type="text" ref={node => this.emailField = node} onChange={e => this.setState({ email: e.target.value })} /></div>
+            <div>Password: <input type="password" ref={node => this.passwordField = node} onChange={e => this.setState({ password: e.target.value })} /></div>
+            <div>
+              <button onClick={e => this.login()}>LOGIN</button>
+              <button onClick={e => this.signup()}>SIGNUP</button>
+            </div>
+          </div>
+        }
       </div>
     );
   }
